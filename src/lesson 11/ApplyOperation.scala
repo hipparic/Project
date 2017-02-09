a7f856be22a3c6e0391fefcb8a530b7c8dc04d6b
hipparchus <1003970780@qq.com>
2017-02-09 16:24:09 +0800

    lesson 10

---
+++ src/lesson 11/ApplyOperation.scala
@@ -0,0 +1,28 @@
+package lesson11
+
+/**
+  * Created by hipparchus on 2017/2/9.
+  * apply function
+  */
+
+class ApplyTest{
+  def apply() = println("I am into Spark!")
+  def haveATry: Unit ={
+    println("Have a try on apply!")
+  }
+}
+
+object ApplyTest{
+  def apply() = {
+    println("I am into Scala!")
+    new ApplyTest
+  }
+}
+
+object ApplyOperation {
+  def main(args: Array[String]) {
+    val arr = Array(1,2,3,4,5)
+    val a = ApplyTest()
+    a.haveATry
+  }
+}
